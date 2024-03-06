470efe1c8ddc41b676ef2e302444c87e722554e4
Kiarod Pashminehazar <kpashminehazar@mednet.ucla.edu>
2024-03-06 14:10:13 -0800

    fix linter warnings in test/assert-* scripts

--- test/assert_MToolBox_csv.sh
+++ test/assert_MToolBox_csv.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # this function cuts around sample_ids so the contents of the CSV can be compared in an sample_id agnostic way
 function trim_and_md5 {
-    cat $1 | cut -f 2 -d ',' | md5sum | cut -f 1 -d ' '
+    cat "$1" | cut -f 2 -d "," | md5sum | cut -f 1 -d " "
 }
 
-received=$(trim_and_md5 $1)
-expected=$(trim_and_md5 $2)
+received=$(trim_and_md5 "$1")
+expected=$(trim_and_md5 "$2")
 
 if [ "$received" == "$expected" ]; then
     echo "csv files are equal"
