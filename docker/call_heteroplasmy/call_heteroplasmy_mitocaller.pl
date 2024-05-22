5f0d554c62648a3bf9275c39140e8f2e6f043363
Mao Tian <maotian@mednet.ucla.edu>
2024-05-21 17:40:25 -0700

    add tumor purity option

--- docker/call_heteroplasmy/call_heteroplasmy_mitocaller.pl
+++ docker/call_heteroplasmy/call_heteroplasmy_mitocaller.pl
@@ -2,8 +2,8 @@
 
 ### call_heteroplasmy_mitocaller.pl
 ##################################################################################
-# 
-# 
+#
+#
 ### HISTORY
 #######################################################################################
 # Version               Date                Coder                     Comments
@@ -58,12 +58,13 @@ sub main {
 		"ascat_stat:s"       => \$opts{'ascat_stat'},
 		"min_coverage=s"     => \$opts{'min_coverage'},
 		"het_fraction=s"     => \$opts{'het_fraction'},
+		"tumour_purity=s"    => \$opts{'tumour_purity'},
 		"output=s"     => \$opts{'output'}
 	) or pod2usage(64);
-	
+
 		if ($opts{'help'}) { pod2usage(1) };
 		if ($opts{'man'}) { pod2usage(-exitstatus => 0, -verbose => 2) };
-	
+
 	while(my ($arg, $value) = each(%opts)){
 		if (!($arg=~/\:/) and !defined $value) {
 		print "ERROR: Missing argument $arg\n";
@@ -72,7 +73,7 @@ sub main {
 		}
 
 		my ($data, $tumour_purity);
-					
+
 		$data = read_table($opts{normal_table}, 'normal', $data);
 		$data = read_table($opts{tumour_table}, 'tumour', $data);
 		#print header
@@ -108,13 +109,15 @@ sub main {
 				}
 
 			close $fh_ascat_stat;
-			
-			} else {
-				print "No ascat stat found and purity estimation will be 1\n";
-			}
+				} elsif ($opts{tumour_purity}) {
+					$tumour_purity = $opts{tumour_purity};
+				} else {
+					print "No ascat stat found and purity estimation will be 1\n";
+					$tumour_purity = 1;
+				}
 
 		#print $fh_out join("\t", 'Position', @samples)."\n";
-		my $header = join("\t", 'chr', 'pos', 'ref', 'normal_genotype', 'tumour_genotype', 
+		my $header = join("\t", 'chr', 'pos', 'ref', 'normal_genotype', 'tumour_genotype',
 			'normal_filtered_depth', 'tumour_filtered_depth',
 			'normal_frequency', 'tumour_frequency',
 			'alt', 'heteroplasmy_fraction_diff', 'heteroplasmy_fraction_adjusted_diff', 'somatic'
@@ -129,7 +132,7 @@ sub main {
 			my $hetp;
 
 			if($data->{normal}->{$pos}->{genotype} ne $data->{tumour}->{$pos}->{genotype}){
-			# print join("\t", $pos, $data->{normal}->{$pos}->{genotype}, $data->{tumour}->{$pos}->{genotype}, $data->{normal}->{$pos}->{fraction}, $data->{tumour}->{$pos}->{fraction})."\n";        
+			# print join("\t", $pos, $data->{normal}->{$pos}->{genotype}, $data->{tumour}->{$pos}->{genotype}, $data->{normal}->{$pos}->{fraction}, $data->{tumour}->{$pos}->{fraction})."\n";
 
 				$hetp = assign_genotype_fraction($data->{normal}->{$pos}->{genotype}, $data->{normal}->{$pos}->{fraction}, 'normal', $hetp);
 				$hetp = assign_genotype_fraction($data->{tumour}->{$pos}->{genotype}, $data->{tumour}->{$pos}->{fraction}, 'tumour', $hetp);
@@ -141,36 +144,36 @@ sub main {
 					if(!$hetp->{normal}->{$base}){
 
 						$hetp->{normal}->{$base} = 0;
-						
+
 						}
 
 					if(!$hetp->{tumour}->{$base}){
 
 						$hetp->{tumour}->{$base} = 0;
-						
+
 						}
 
 					### adjust HF using tumour purity ######################################################################################
 					$hetp->{tumour_adjusted}->{$base} = get_adjusted_hf($hetp->{normal}->{$base}, $hetp->{tumour}->{$base}, $tumour_purity);
 
 					if(($hetp->{tumour_adjusted}->{$base} - $hetp->{normal}->{$base}) > $opts{het_fraction}){
-						
+
 						my $val = ($hetp->{tumour}->{$base} - $hetp->{normal}->{$base});
 						my $val_adjusted = ($hetp->{tumour_adjusted}->{$base} - $hetp->{normal}->{$base});
 						#print $pos."\t".$base."\t".$hetp->{normal}->{$base}."\t".$hetp->{tumour}->{$base}."\t".$data->{tumour}->{$pos}->{val}."\n";
-						$data->{tumour}->{$pos}->{somaticheteroplasmy_binary} = 1; 
+						$data->{tumour}->{$pos}->{somaticheteroplasmy_binary} = 1;
 						push @{$data->{tumour}->{$pos}->{somaticheteroplasmy_base}}, $base;
 						push @{$data->{tumour}->{$pos}->{somaticheteroplasmy_val}}, $val;
 						push @{$data->{tumour}->{$pos}->{somaticheteroplasmy_val_adjusted}}, $val_adjusted;
-						} 
+						}
 
 					}
 				}
-				
+
 				#In the RSRS (Reconstructed Sapiens Reference Sequence) reference genome, 523, 524 and 3107 are N's used to preserve historical numbering
-				#In the rCRS (revised Cambridge Reference Sequence) genome, only 3107 is an N. 523-524 are AC. However these positions are commonly deleted which is why they are not in RSRS. 
+				#In the rCRS (revised Cambridge Reference Sequence) genome, only 3107 is an N. 523-524 are AC. However these positions are commonly deleted which is why they are not in RSRS.
 				#Position 310, a homopolymer stretch is commonly misaligned, is removed from consideration as well.
-				
+
 				if(!$data->{tumour}->{$pos}->{somaticheteroplasmy_binary} or $pos =~m/(^310$|^523$|^524$|^3107$)/ or $data->{normal}->{$pos}->{filt_depth} < $opts{min_coverage} or $data->{tumour}->{$pos}->{filt_depth} < $opts{min_coverage}){
 					$data->{tumour}->{$pos}->{somaticheteroplasmy_binary} = 0;
 					}
@@ -182,9 +185,9 @@ sub main {
 					push @{$data->{tumour}->{$pos}->{somaticheteroplasmy_val_adjusted}}, 'NA';
 					}
 
-				my $output = join("\t", $data->{normal}->{$pos}->{chr}, $pos, $data->{normal}->{$pos}->{ref}, 
-					$data->{normal}->{$pos}->{genotype}, 
-					$data->{tumour}->{$pos}->{genotype}, 
+				my $output = join("\t", $data->{normal}->{$pos}->{chr}, $pos, $data->{normal}->{$pos}->{ref},
+					$data->{normal}->{$pos}->{genotype},
+					$data->{tumour}->{$pos}->{genotype},
 					$data->{normal}->{$pos}->{filt_depth},
 					$data->{tumour}->{$pos}->{filt_depth},
 					$data->{normal}->{$pos}->{fraction},
@@ -197,7 +200,7 @@ sub main {
 
 				if($data->{tumour}->{$pos}->{somaticheteroplasmy_binary} eq 1){
 					print $fh_filt_out $output;
-					} 
+					}
 
 					print $fh_out $output;
 			}
@@ -249,11 +252,11 @@ sub read_table {
 	print "Processing $file\n";
 
 		open (my $fh_file, "gunzip -c $file |");
-		
+
 		my $header = <$fh_file>;
 
 		my $col_idx_tsv = column_map($header);
-		
+
 		while(<$fh_file>){
 
 				my $line = $_;
@@ -267,13 +270,13 @@ sub read_table {
 				$data->{$sample}->{$pos}->{filt_depth} = $fields[$col_idx_tsv->{'FilteredDepth'}];
 				$data->{$sample}->{$pos}->{genotype} = $fields[$col_idx_tsv->{'Genotype'}];
 				$data->{$sample}->{$pos}->{fraction} = $fields[$col_idx_tsv->{'AlleleFractions'}];
-				
+
 				$data->{$sample}->{$pos}->{ref} =~s/RefBase://;
 				$data->{$sample}->{$pos}->{filt_depth} =~s/DepthFilter://;
 				$data->{$sample}->{$pos}->{genotype} =~s/Genotype://;
 				$data->{$sample}->{$pos}->{fraction} =~s/Frequency://;
 			}
-		
+
 		close ($fh_file);
 
 		return($data);
@@ -289,7 +292,7 @@ sub column_map {
 		my $idx = 0;
 		my $col_idx;
 		map { my $colname = $_; $col_idx->{$colname} = $idx; ++$idx;} @header;
-		
+
 		#show index
 		$Data::Dumper::Sortkeys = 1;
 		#print Dumper($col_idx);
